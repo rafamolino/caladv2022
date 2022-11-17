2d19504755404afb80533c8341706600b58301b4
rafamolino <35599091+rafamolino@users.noreply.github.com>
2022-11-17 21:21:07 +0100

    sin errores

---
+++ index.js
@@ -0,0 +1,16 @@
+function PlaySound(soundobj) {
+        
+    var thissound = document.getElementById(soundobj);
+
+    thissound.play();
+
+}
+
+
+function PauseSound(soundobj) {
+
+    var thissound = document.getElementById(soundobj);
+
+    thissound.pause();
+
+}
